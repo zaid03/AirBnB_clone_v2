0f91ce14f93c65a6efdedac4d12187ff21cb2796
zaid03 <terguyzaid@gmail.com>
2024-02-17 13:36:02 +0100

    mods

--- models/state.py
+++ models/state.py
@@ -11,7 +11,15 @@ from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Defines State class"""
+    """Represents a state for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
@@ -19,7 +27,7 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Getter attribute for cities in FileStorage"""
+            """Get a list of all related City objects."""
             city_list = []
             for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
